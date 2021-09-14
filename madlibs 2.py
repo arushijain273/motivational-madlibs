72a6c7a294f761c7f9c2d858e0975340f844298c
Arushi Jain <69908093+arushijain273@users.noreply.github.com>
2021-09-14 12:24:22 +0530

    Update madlibs 2.py

--- madlibs 2.py
+++ madlibs 2.py
@@ -1,12 +1,11 @@
-from tkinter import *
 import datetime
 
 name= input("Whats your name hun? ")
 workVerb= input("What are you passionate about bub? ")
 roleModel= input("Who do you look up to?" )
 goodAdj= input("Gimme a good adjective :)" )
-
-madlibs= f"Hey {name}, everything will be alright. You'll be back to your work of {workVerb}.\nYou're a genius. {roleModel} believes in you and so does this {goodAdj} programmer.\nYOU GOT THIS!!"
 now= datetime.datetime.today()
+
+madlibs= f"Hey {name}, everything will be alright from this {now} instance. You'll be back to your work of {workVerb}.\nYou're a genius. {roleModel} believes in you and so does this {goodAdj} programmer.\nYOU GOT THIS!!"
+
 print(madlibs)
-print(now)
